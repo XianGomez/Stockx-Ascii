6dcd5619b75f5446eee36fc80dec262294276953
XianGomez <xiiangomezz@gmail.com>
2026-05-08 14:05:28 +0200

    feat(Bid): Añadir clase Bid primera historia usuario

---
+++ app/src/main/java/edu/teamrocket/Offer/Bid.java
@@ -0,0 +1,23 @@
+package edu.teamrocket.Offer;
+
+public class Bid {
+    private String size;
+    private Integer bid;
+
+    Bid(String size, int bid) {
+        this.size = size;
+        this.bid = bid;
+    }
+
+    public String size() {
+        return this.size;
+    }
+
+    public int value() {
+        return this.bid;
+    }
+
+    public int compareTo(Offer offer) {
+        return 0;
+    }
+}
