89ba2796f82ce075af0039c3bec8e993304a29d0
XianGomez <xiiangomezz@gmail.com>
2026-05-08 14:00:48 +0200

    feat(Item): Añadir Item primera historia de usuario

---
+++ app/src/main/java/edu/teamrocket/Item/Item.java
@@ -0,0 +1,17 @@
+package edu.teamrocket.Item;
+
+import java.util.List;
+
+import edu.teamrocket.Offer.Offer;
+
+public interface Item {
+    public int getBid();
+    public int getAsk();
+    public int getSale();
+    public void setBid(int bid);
+    public void setAsk(int ask);
+    public void setSale(int sale);
+    
+
+    
+}
