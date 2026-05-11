67d1bbe0d4654f0b6d81ab45674ace8e3ab10582
XianGomez <xiiangomezz@gmail.com>
2026-05-11 13:40:35 +0200

    feat(Item): Añadir metodo add

--- app/src/main/java/edu/teamrocket/Item/Item.java
+++ app/src/main/java/edu/teamrocket/Item/Item.java
@@ -8,6 +8,7 @@ public interface Item {
     public int getBid();
     public int getAsk();
     public int getSale();
+    public void add(Offer offer);
     public void setBid(int bid);
     public void setAsk(int ask);
     public void setSale(int sale);
