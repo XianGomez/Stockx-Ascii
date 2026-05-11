75e7ed2a951f5f207545d1bc80fb4d51317f4964
XianGomez <xiiangomezz@gmail.com>
2026-05-11 13:38:36 +0200

    feat(Sneaker): Añadir Lista offerList y metodo add

--- app/src/main/java/edu/teamrocket/Item/Sneaker.java
+++ app/src/main/java/edu/teamrocket/Item/Sneaker.java
@@ -1,4 +1,5 @@
 package edu.teamrocket.Item;
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.teamrocket.Offer.Offer;
@@ -10,9 +11,12 @@ public class Sneaker implements Item {
     private int ask;
     private int bid;
 
+    private List<Offer> offerList;
+
     public Sneaker(String style, String name) {
         this.style = style;
         this.name = name;
+        this.offerList = new ArrayList<>();
     }
 
     
@@ -52,6 +56,10 @@ public class Sneaker implements Item {
     @Override
     public void setAsk(int ask) {
         this.ask = ask;
+    }
+
+    public void add(Offer offer) {
+        offerList.add(offer);
     }
 
     @Override
