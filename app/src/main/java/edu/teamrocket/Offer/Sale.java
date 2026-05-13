bd2a9b845befa2735f51c4e3b28d0686fd9188c5
XianGomez <xiiangomezz@gmail.com>
2026-05-13 14:14:43 +0200

    feat(Sale): Añadir logica Sale

---
+++ app/src/main/java/edu/teamrocket/Offer/Sale.java
@@ -0,0 +1,29 @@
+package edu.teamrocket.Offer;
+
+public class Sale implements Offer {
+    private String size;
+    private Integer price;
+
+    public Sale(String size, int price) {
+        this.size = size;
+        this.price = price;
+    }
+
+    public String size() {
+        return this.size;
+    }
+
+    public int value() {
+        return this.price;
+    }
+
+    @Override
+    public int compareTo(Offer sale) {
+        return Integer.compare(this.value(), sale.value());
+    }
+
+    @Override
+    public String toString() {
+        return size() + "\t" + value();
+    }
+}
