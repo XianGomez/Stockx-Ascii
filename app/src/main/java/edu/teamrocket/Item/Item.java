02c77d6107418b38b713c732d8a5a4aa75bd4bb8
XianGomez <xiiangomezz@gmail.com>
2026-05-13 10:23:33 +0200

    feat(Item): Añadir logica metodo offers

--- app/src/main/java/edu/teamrocket/Item/Item.java
+++ app/src/main/java/edu/teamrocket/Item/Item.java
@@ -9,6 +9,7 @@ public interface Item {
     public int getAsk();
     public int getSale();
     public void add(Offer offer);
+    public List<Offer> offers();
     public void setBid(int bid);
     public void setAsk(int ask);
     public void setSale(int sale);
