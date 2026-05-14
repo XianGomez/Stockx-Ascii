27d99441ac09625943c3cde8222c9624d395a45d
XianGomez <xiiangomezz@gmail.com>
2026-05-14 10:37:53 +0200

    feat(Size): Añadir logica size

---
+++ app/src/main/java/edu/teamrocket/Criteria/Size.java
@@ -0,0 +1,30 @@
+package edu.teamrocket.Criteria;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.teamrocket.Item.Item;
+import edu.teamrocket.Offer.Offer;
+import edu.teamrocket.Offer.Sale;
+
+public class Size implements Criteria {
+
+    private String size;
+    public Size(String size) {
+        this.size = size;
+    };
+
+    @Override
+    public List<Offer> checkCriteria(Item item) {
+        List<Offer> result = new ArrayList<>();
+
+        for (Offer offer : item.offers()) {
+            if (offer.size().equals(size)) {
+                result.add(offer);
+            }
+        }
+
+        return result;
+    }
+    
+}
